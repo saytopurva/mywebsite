48af0286126ed9fddf51b064d55cc9c29905fc88
purva pawar <purvapramodpawar712@gmail.com>
2025-02-16 01:53:20 +0530

    k

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
     ScrollReveal().reveal(".header a, .profile-photo, .about-content, .education", {
       origin: "left"
     });
-    ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .internship", {
+    ScrollReveal().reveal(".header ul, .profile-text, .about-skills, .skill", {
       origin: "right"
     });
     ScrollReveal().reveal(".project-title, .contact-title", {
